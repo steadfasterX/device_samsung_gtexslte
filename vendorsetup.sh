716bf04cda97fafc85679eb8ee6d1974589f83d5
stay steadfast <steadfasterX@users.noreply.github.com>
2020-10-26 13:17:31 +0100

    gtexslte: initial bringup of /e/

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,16 @@
 add_lunch_combo lineage_gtexslte-userdebug
 add_lunch_combo lineage_gtexslte-eng
+
+############# /e/ section ###############
+
+export EOS_DEVICE=gtexslte
+export EOS_BRANCH_NAME="v1-nougat"
+export EOS_RELEASE_TYPE="UNOFFICIAL"
+#export EOS_CUSTOM_PACKAGES="MuPDF GmsCore GsfProxy FakeStore com.google.android.maps.jar Mail BlissLauncher BlissIconPack MozillaNlpBackend OpenWeatherMapWeatherProvider AccountManager MagicEarth OpenCamera eDrive Weather Notes Tasks NominatimNlpBackend DroidGuard OpenKeychain Message Browser BrowserWebView LibreOfficeViewer noEOSappstore"
+export EOS_OTA_CLEARTEXT=true
+export EOS_USE_CCACHE=true
+export EOS_CCACHE_DIR=/tmp/ccache/jenkins/14.1
+export EOS_CCACHE_SIZE=10G
+export EOS_TMP_DIR=/tmp/eos
+export EOS_ZIP_DIR=/tmp/eos/zips
+export EOS_CLEAN_ZIPDIR=false
