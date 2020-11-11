f326bda870d982d9c5992372c38ddb04e99e9c31
steadfasterX <steadfasterX@gmail.com>
2020-11-11 16:59:25 +0100

    gtexslte: vendorsetup: ccache compress
    Signed-off-by: steadfasterX <steadfasterX@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,6 +9,7 @@ export EOS_RELEASE_TYPE="UNOFFICIAL"
 #export EOS_CUSTOM_PACKAGES="MuPDF GmsCore GsfProxy FakeStore com.google.android.maps.jar Mail BlissLauncher BlissIconPack MozillaNlpBackend OpenWeatherMapWeatherProvider AccountManager MagicEarth OpenCamera eDrive Weather Notes Tasks NominatimNlpBackend DroidGuard OpenKeychain Message Browser BrowserWebView LibreOfficeViewer noEOSappstore"
 export EOS_OTA_CLEARTEXT=true
 export EOS_USE_CCACHE=true
+export EOS_CCACHE_COMPRESS=true
 export EOS_CCACHE_DIR=/tmp/ccache/jenkins/14.1
 export EOS_CCACHE_SIZE=10G
 export EOS_TMP_DIR=/tmp/eos
